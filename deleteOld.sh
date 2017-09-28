f2e7aa831cb744b191938d3aa4ab8c14424ccd88
Jens Giehl <mobil.jgiehl@gmail.com>
2017-09-28 22:42:04 +0200

    Renamded sendMail.sh -> deleteOld.sh

---
+++ deleteOld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# example call: ./sendMail.sh 'myFolder/myFile_*'
+if [ $# -eq 1 ]
+then
+  diffName="diff.png"
+  array=($( ls -t $1  | head -2 ))
+  if [ -z ${array[1]} ];
+  then
+      diffValue=$( compare -metric MAE ${array[0]} ${array[1]} $diffName 2>&1 |tr -d '.'|cut -f1 -d' ' )
+      logger "Compare ${array[0]} with ${array[1]}: Diff is $diffValue"
+  fi
+
