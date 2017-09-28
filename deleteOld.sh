e1b2d6a31956780bd35e16e2ddfc4c37b5b87cd9
Jens Giehl <mobil.jgiehl@gmail.com>
2017-09-29 00:09:18 +0200

    WIP

--- deleteOld.sh
+++ deleteOld.sh
@@ -2,11 +2,12 @@
 # example call: ./sendMail.sh 'myFolder/myFile_*'
 if [ $# -eq 1 ]
 then
-  diffName="diff.png"
+  diffName="diff" + `date '+%Y-%m-%d_%H%M'` +".png"
   array=($( ls -t $1  | head -2 ))
   if [ -z ${array[1]} ];
   then
-      diffValue=$( compare -metric MAE ${array[0]} ${array[1]} $diffName 2>&1 |tr -d '.'|cut -f1 -d' ' )
+      diffValue=$( compare -metric MAE ${array[0]} ${array[1]} $1/$diffName 2>&1 |tr -d '.'|cut -f1 -d' ' )
       logger "Compare ${array[0]} with ${array[1]}: Diff is $diffValue"
+      echo $diffValue
   fi
-
+fi
--- takeScreenshots.sh
+++ takeScreenshots.sh
@@ -6,11 +6,36 @@ logger "Save screenshots in $2"
 IFS='.' read -ra inputFile <<< $1
 folder=${inputFile[0]}
 
-# Read input file 
+# Read input file
 while read line; do
     IFS=';' read -ra JOB <<< $line
 
     # Take mobile and desktop screenshot
     phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/desktop/$folder/" DESKTOP
-    phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/mobile/$folder/" MOBILE
+#    phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/mobile/$folder/" MOBILE
+
+	# Remove duplications
+	diffName=`date '+%Y-%m-%d_%H%M'`
+	diffName="diff_$diffName.png"
+	desktopFolder="$2/${JOB[0]}/desktop/$folder/"
+	mobileFolder="$2/${JOB[0]}/mobile/$folder/"
+	
+	array=($( ls -t "$desktopFolder"  | head -2 ))
+	if [ ${#array[@]} -gt 1 ];
+	then
+		diffValue=$( compare -metric MAE "$desktopFolder/${array[0]}" "$desktopFolder/${array[1]}" $diffName 2>&1 |head -1 |tr -d '.'|cut -f1 -d' ' )
+		logger "Compare ${array[0]} with ${array[1]}: Diff is $diffValue"
+		if [ $diffValue == "0" ];
+		then
+			echo "Delete $desktopFolder/${array[0]}"
+			# Remove newer screenshot
+			rm "$desktopFolder/${array[0]}"
+			rm $diffName
+		else
+			mkdir --parents "$desktopFolder/diff/"
+			mv $diffName "$desktopFolder/diff/$diffName"
+		fi
+	fi
+
 done < $1
+
