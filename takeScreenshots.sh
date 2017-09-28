61002d8568ccfb570f677618b9c93fb446179aec
Jens Giehl <mobil.jgiehl@gmail.com>
2017-09-29 00:40:45 +0200

    Added function

--- takeScreenshots.sh
+++ takeScreenshots.sh
@@ -2,6 +2,38 @@
 logger "Read file $1"
 logger "Save screenshots in $2"
 
+function takeScreenshot() {
+	url=$1
+	folder=$2
+	agent=$3
+
+	# Take screenshot
+	phantomjs takeScreenshot.js $1 "$folder" $3
+
+
+        diffName=`date '+%Y-%m-%d_%H%M'`
+        diffName="diff_$diffName.png"
+
+        # Compare screenshot with an older version
+        array=($( ls -t "$folder"  | head -2 ))
+        if [ ${#array[@]} -gt 1 ];
+        then
+                diffValue=$( compare -metric MAE "$folder/${array[0]}" "$folder/${array[1]}" $diffName 2>&1 |head -1 |tr -d '.'|cut -f1 -d' ' )
+                logger "Compare ${array[0]} with ${array[1]}: Diff is $diffValue"
+                if [ $diffValue == "0" ];
+                then
+                        # Remove newer screenshot
+			echo "Delete $folder/${array[0]}"
+                        rm "$folder/${array[0]}"
+                        rm $diffName
+                else
+                        mkdir --parents "$folder/diff/"
+                        mv $diffName "$folder/diff/$diffName"
+                fi
+        fi
+}
+
+
 # Use filename as subfolder for the output files
 IFS='.' read -ra inputFile <<< $1
 folder=${inputFile[0]}
@@ -11,31 +43,12 @@ while read line; do
     IFS=';' read -ra JOB <<< $line
 
     # Take mobile and desktop screenshot
-    phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/desktop/$folder/" DESKTOP
+    takeScreenshot ${JOB[1]} "$2/${JOB[0]}/desktop/$folder/" DESKTOP
+
+
+#    phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/desktop/$folder/" DESKTOP
 #    phantomjs takeScreenshot.js ${JOB[1]} "$2/${JOB[0]}/mobile/$folder/" MOBILE
 
-	# Remove duplications
-	diffName=`date '+%Y-%m-%d_%H%M'`
-	diffName="diff_$diffName.png"
-	desktopFolder="$2/${JOB[0]}/desktop/$folder/"
-	mobileFolder="$2/${JOB[0]}/mobile/$folder/"
-	
-	array=($( ls -t "$desktopFolder"  | head -2 ))
-	if [ ${#array[@]} -gt 1 ];
-	then
-		diffValue=$( compare -metric MAE "$desktopFolder/${array[0]}" "$desktopFolder/${array[1]}" $diffName 2>&1 |head -1 |tr -d '.'|cut -f1 -d' ' )
-		logger "Compare ${array[0]} with ${array[1]}: Diff is $diffValue"
-		if [ $diffValue == "0" ];
-		then
-			echo "Delete $desktopFolder/${array[0]}"
-			# Remove newer screenshot
-			rm "$desktopFolder/${array[0]}"
-			rm $diffName
-		else
-			mkdir --parents "$desktopFolder/diff/"
-			mv $diffName "$desktopFolder/diff/$diffName"
-		fi
-	fi
-
 done < $1
+
 
